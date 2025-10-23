c97ef0c6b84a671cfb6058d93081413fcbc74659
Zacknetic <zackavino@gmail.com>
2025-10-23 16:13:59 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/zacknetic/zoomintegration/ZoomintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.zacknetic.zoomintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zacknetic/zoomintegration/ZoomintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacknetic.zoomintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
